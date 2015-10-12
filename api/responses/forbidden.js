058e537699886ff5e2bcbd089627396ffe3666ac
Will <whp3652@rit.edu>
2015-10-11 21:03:57 -0400

    Update responses to be JSON only

--- api/responses/forbidden.js
+++ api/responses/forbidden.js
@@ -1,76 +1,41 @@
 /**
- * 403 (Forbidden) Handler
- *
- * Usage:
- * return res.forbidden();
- * return res.forbidden(err);
- * return res.forbidden(err, 'some/specific/forbidden/view');
- *
- * e.g.:
- * ```
- * return res.forbidden('Access denied.');
- * ```
- */
+* 403 (Forbidden) Handler
+*
+* @description
+* The request was a legal request, but the server is refusing to respond to it.
+* Unlike a 401 Unauthorized response, authenticating will make no difference.
+* Error code for user not authorized to perform the operation or the resource
+* is unavailable for some reason.
+*
+* @param {Object} [data] arbitrary data object
+* @param {Object} [options]
+* @param {String} [options.code]
+* @param {String} [options.message]
+* @param {Object} [options.root]
+*
+* @see {@link ErrorCodeService}
+* {@link https://github.com/IncoCode/sails-service-error-codes#default-error-codes Default error code definitions}
+*
+* @example
+* return res.forbidden();
+* return res.forbidden(data);
+* return res.forbidden(data, {message: 'I'm sorry Dave, I'm afraid I can't do that'});
+*/
+
+const ErrorCodes = require('../services/ErrorCodeService');
+const _ = require('lodash');
 
 module.exports = function forbidden(data, options) {
 
-  // Get access to `req`, `res`, & `sails`
-  const req = this.req;
-  const res = this.res;
-  const sails = req._sails;
+  const defaults = {
+    code: ErrorCodes.forbidden.code,
+    message: ErrorCodes.forbidden.message,
+    data: data || {},
+  };
 
-  // Set status code
-  res.status(403);
+  const response = _.defaults(options || {}, defaults);
 
-  // Log error to console
-  if (data !== undefined) {
-    sails.log.verbose('Sending 403 ("Forbidden") response: \n', data);
-  }
-  else sails.log.verbose('Sending 403 ("Forbidden") response');
-
-  // Only include errors in response if application environment
-  // is not set to 'production'.  In production, we shouldn't
-  // send back any identifying information about errors.
-  if (sails.config.environment === 'production') {
-    data = undefined;
-  }
-
-  // If the user-agent wants JSON, always respond with JSON
-  if (req.wantsJSON) {
-    return res.jsonx(data);
-  }
-
-  // If second argument is a string, we take that to mean it refers to a view.
-  // If it was omitted, use an empty object (`{}`)
-  options = (typeof options === 'string') ? { view: options } : options || {};
-
-  // If a view was provided in options, serve it.
-  // Otherwise try to guess an appropriate view, or if that doesn't
-  // work, just send JSON.
-  if (options.view) {
-    return res.view(options.view, { data: data });
-  }
-
-  // If no second argument provided, try to serve the default view,
-  // but fall back to sending JSON(P) if any errors occur.
-  else {
-    return res.view('403', { data: data }, (err, html) => {
-      // If a view error occured, fall back to JSON(P).
-      if (err) {
-        // Additionally:
-        // • If the view was missing, ignore the error but provide a verbose log.
-        if (err.code === 'E_VIEW_FAILED') {
-          sails.log.verbose('res.forbidden() :: Could not locate view for error page (sending JSON instead).  Details: ', err);
-        }
-        // Otherwise, if this was a more serious error, log to the console with the details.
-        else {
-          sails.log.warn('res.forbidden() :: When attempting to render error page view, an error occured (sending JSON instead).  Details: ', err);
-        }
-        return res.jsonx(data);
-      }
-
-      return res.send(html);
-    });
-  }
+  this.res.status(403);
+  this.res.jsonx(response);
 };
 
--- api/responses/serverError.js
+++ api/responses/serverError.js
@@ -1,75 +1,47 @@
 /**
- * 500 (Server Error) Response
- *
- * Usage:
- * return res.serverError();
- * return res.serverError(err);
- * return res.serverError(err, 'some/specific/error/view');
- *
- * NOTE:
- * If something throws in a policy or controller, or an internal
- * error is encountered, Sails will call `res.serverError()`
- * automatically.
- */
+* 500 (Internal Server Error) Response
+*
+* @description
+* A generic error message, given when no more specific message is suitable.
+* The general catch-all error when the server-side throws an exception.
+*
+* @param {Object} [data] arbitrary data object
+* @param {Object} [options]
+* @param {String} [options.code]
+* @param {String} [options.message]
+* @param {Object} [options.root]
+*
+* @see {@link ErrorCodeService}
+* {@link https://github.com/IncoCode/sails-service-error-codes#default-error-codes Default error code definitions}
+*
+* @example
+* return res.serverError();
+* return res.serverError(data);
+* return res.serverError(data, {message: 'Uh-oh'});
+*
+* NOTE:
+* If something throws in a policy or controller, or an internal
+* error is encountered, Sails will call `res.serverError()`
+* automatically.
+*/
+
+const ErrorCodes = require('../services/ErrorCodeService');
+const _ = require('lodash');
 
 module.exports = function serverError(data, options) {
 
-  // Get access to `req`, `res`, & `sails`
-  const req = this.req;
-  const res = this.res;
-  const sails = req._sails;
-
-  // Set status code
-  res.status(500);
+  const defaults = {
+    code: ErrorCodes.serverError.code,
+    message: ErrorCodes.serverError.message,
+    data: data || {},
+  };
 
   // Log error to console
-  if (data !== undefined) {
-    sails.log.error('Sending 500 ("Server Error") response: \n', data);
-  }
-  else sails.log.error('Sending empty 500 ("Server Error") response');
+  sails.log.error(500, data);
 
-  // Only include errors in response if application environment
-  // is not set to 'production'.  In production, we shouldn't
-  // send back any identifying information about errors.
-  if (sails.config.environment === 'production') {
-    data = undefined;
-  }
+  const response = _.defaults(options || {}, defaults);
 
-  // If the user-agent wants JSON, always respond with JSON
-  if (req.wantsJSON) {
-    return res.jsonx(data);
-  }
-
-  // If second argument is a string, we take that to mean it refers to a view.
-  // If it was omitted, use an empty object (`{}`)
-  options = (typeof options === 'string') ? { view: options } : options || {};
-
-  // If a view was provided in options, serve it.
-  // Otherwise try to guess an appropriate view, or if that doesn't
-  // work, just send JSON.
-  if (options.view) {
-    return res.view(options.view, { data: data });
-  }
-  // If no second argument provided, try to serve the default view,
-  // but fall back to sending JSON(P) if any errors occur.
-  else {
-    return res.view('500', { data: data }, (err, html) => {
-      // If a view error occured, fall back to JSON(P).
-      if (err) {
-        // Additionally: If the view was missing, ignore the error but provide
-        // a verbose log.
-        if (err.code === 'E_VIEW_FAILED') {
-          sails.log.verbose('res.serverError() :: Could not locate view for error page (sending JSON instead).  Details: ', err);
-        }
-        // Otherwise, if this was a more serious error, log to the console
-        // with the details.
-        else {
-          sails.log.warn('res.serverError() :: When attempting to render error page view, an error occured (sending JSON instead).  Details: ', err);
-        }
-        return res.jsonx(data);
-      }
-      return res.send(html);
-    });
-  }
+  this.res.status(500);
+  this.res.jsonx(response);
 };
 
