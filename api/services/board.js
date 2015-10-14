468fb3bc091c64b7ec4f95bc01e68dd421d6d5ca
Brax <baf9201@rit.edu>
2015-10-14 11:40:18 -0400

    Stubbed out a Board Service

---
+++ api/services/board.js
@@ -0,0 +1,41 @@
+/**
+ * Board Service
+ *
+ * @file Contains logic for Board actions
+ * @module Services/Board
+ */
+
+const board = {};
+
+/**
+ * Create a new Board
+ */
+board.create = function() {
+};
+
+/**
+ * Join a socket into a socket.io room
+ */
+board.join = function() {
+};
+
+/**
+ * Move a socket out of a socket.io room
+ */
+board.leave = function() {
+};
+
+/**
+ * Add a User to a Board
+ */
+board.add = function() {
+};
+
+/**
+ * Remove a User from a Board
+ */
+board.remove = function() {
+};
+
+
+module.exports = board;
