a8161c0a9ecde3053b6a09044c0b82e5bd25361a
Eric Kipnis <esk8107@rit.edu>
2015-10-24 15:30:04 -0400

    Add all routes in Board Controller and Services.
    Fix indentation errors
    Replace Board Service code redundancy.

--- api/services/BoardService.js
+++ api/services/BoardService.js
@@ -20,165 +20,189 @@ board.leave = function() {
 // Remove a board from the database
 board.destroy = function(boardId) {
 
-	return Board.destroy({boardId: boardId});
+  return Board.destroy({boardId: boardId});
 };
 
 // Add a user to the board
 board.addUser = function(boardId, userObj) {
 
-	Board.findOne({boardId: boardId}).populate('users')
+  board.findBoardAndPopulate(boardId, 'users')
 
-	.then(function(found) {
+  .then(function(found) {
 
-		found.users.add(userObj);
+    found.users.add(userObj);
 
-		return found.save();
-	})
-	.catch(function(err) {
+    return found.save();
+  })
+  .catch(function(err) {
 
-		throw new Error(err);
-	});
+    throw new Error(err);
+  });
 };
 
 // Remove a user from the board
 board.removeUser = function(boardId, userObj) {
 
-	Board.findOne({boardId: boardId}).populate('users')
+  board.findBoardAndPopulate(boardId, 'users')
 
-	.then(function(found) {
+  .then(function(found) {
 
-		found.users.remove(userObj);
+    found.users.remove(userObj);
 
-		return found.save();
-	})
-	.catch(function(err) {
+    return found.save();
+  })
+  .catch(function(err) {
 
-		throw new Error(err);
-	});
+    throw new Error(err);
+  });
 };
 
 // Add an admin to the board
 board.addAdmin = function(boardId, userObj) {
 
-	Board.findOne({boardId: boardId}).populate('admins')
+  board.findBoardAndPopulate(boardId, 'admins')
 
-	.then(function(found) {
+  .then(function(found) {
 
-		found.admins.add(userObj);
+    found.admins.add(userObj);
 
-		return found.save();
-	})
-	.catch(function(err) {
+    return found.save();
+  })
+  .catch(function(err) {
 
-		throw new Error(err);
-	});
+    throw new Error(err);
+  });
 };
 
 // Remove an admin to the board
 board.removeAdmin = function(boardId, userObj) {
 
-	Board.findOne({boardId: boardId}).populate('admins')
+  board.findBoardAndPopulate(boardId, 'admins')
 
-	.then(function(found) {
+  .then(function(found) {
 
-	found.admins.remove(userObj);
+    found.admins.remove(userObj);
 
-		return found.save();
-	})
-	.catch(function(err) {
+    return found.save();
+  })
+  .catch(function(err) {
 
-		throw new Error(err);
-	});
+    throw new Error(err);
+  });
 };
 
 // Add a pending user to the board
 board.addPendingUser = function(boardId, userObj) {
 
-		Board.findOne({boardId: boardId}).populate('pendingUsers')
+  board.findBoardAndPopulate(boardId, 'pendingUsers')
 
-		.then(function(found) {
+  .then(function(found) {
 
-		found.pendingUsers.add(userObj);
+    found.pendingUsers.add(userObj);
 
-		return found.save();
-	})
-	.catch(function(err) {
+    return found.save();
+  })
+  .catch(function(err) {
 
-		throw new Error(err);
-	});
+    throw new Error(err);
+  });
 };
 
 // Remove a pending user from the board
 board.removePendingUser = function(boardId, userObj) {
 
-	Board.findOne({boardId: boardId}).populate('pendingUsers')
+  board.findBoardAndPopulate(boardId, 'pendingUsers')
 
-	.then(function(found) {
+  .then(function(found) {
 
-		found.pendingUsers.remove(userObj);
+    found.pendingUsers.remove(userObj);
 
-		return found.save();
-	})
-	.catch(function(err) {
+    return found.save();
+  })
+  .catch(function(err) {
 
-		throw new Error(err);
-	});
+    throw new Error(err);
+  });
 };
 
 // Add an idea to the board
 board.addIdea = function(boardId, ideaObj) {
 
-	Board.findOne({boardId: boardId}).populate('ideas')
+  board.findBoardAndPopulate(boardId, 'ideas')
 
-	.then(function(found) {
+  .then(function(found) {
 
-		found.ideas.add(ideaObj);
+    found.ideas.add(ideaObj);
 
-		return found.save();
-	})
-	.catch(function(err) {
+    return found.save();
+  })
+  .catch(function(err) {
 
-		throw new Error(err);
-	});
+    throw new Error(err);
+  });
 };
 
 // Remove an idea to the board
 board.removeIdea = function(boardId, ideaObj) {
 
-	Board.findOne({boardId: boardId}).populate('ideas')
+  board.findBoardAndPopulate(boardId, 'ideas')
 
-	.then(function(found) {
+  .then(function(found) {
 
-		found.ideas.remove(ideaObj);
+    found.ideas.remove(ideaObj);
 
-		return found.save();
-	})
-	.catch(function(err) {
+    return found.save();
+  })
+  .catch(function(err) {
 
-		throw new Error(err);
-	});
+    throw new Error(err);
+  });
 };
 
 // Add an idea collection to the board
 board.addIdeaCollection = function(boardId, ideaCollectionObj) {
 
-	Board.findOne({boardId: boardId}).populate('collections').exec(function(err, found) {
+  board.findBoardAndPopulate(boardId, 'collections')
 
-		found.collections.add(ideaCollectionObj);
+  .then(function(found) {
 
-		return found.save();
-	});
+    found.collections.add(ideaCollectionObj);
+
+    return found.save();
+  })
+  .catch(function(err) {
+
+    throw new Error(err);
+  });
 };
 
 // Remove an idea collection from the board
 board.removeIdeaCollection = function(boardId, ideaCollectionObj) {
 
-	Board.findOne({boardId: boardId}).populate('collections').exec(function(err, found) {
+  board.findBoardAndPopulate(boardId, 'collections')
 
-		found.collections.remove(ideaCollectionObj);
+  .then(function(found) {
 
-		return found.save();
-	});
+    found.collections.remove(ideaCollectionObj);
+
+    return found.save();
+  })
+  .catch(function(err) {
+
+    throw new Error(err);
+  });
+};
+
+// Find a board
+board.findBoard = function(boardId) {
+
+  return Board.findOne({boardId: boardId});
+};
+
+// Find and populate a board with collection string
+board.findBoardAndPopulate = function(boardId, collection) {
+
+  return Board.findOne({boardId: boardId}).populate(collection);
 };
 
 module.exports = board;
