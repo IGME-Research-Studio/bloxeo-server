376cfa78759668389cd5bc4f62802d82dd22344c
Eric Kipnis <esk8107@rit.edu>
2015-11-22 12:23:39 -0500

    Add TimerService for voting timer

--- api/services/RedisService.js
+++ api/services/RedisService.js
@@ -4,7 +4,7 @@
   @file Creates a singleton for a Redis connection
 */
 const Redis = require('ioredis');
-const config = require('../../config.js');
+const config = require('../../config');
 const redisURL = config.redisURL;
 const redis = new Redis(redisURL);
 
