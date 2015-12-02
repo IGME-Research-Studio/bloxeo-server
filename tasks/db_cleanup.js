6c81d540763d6b17b9bd5614800a1f3c2989537c
Brax <baf9201@rit.edu>
2015-12-02 11:50:06 -0500

    Setup npm cleanup script and task dir. Stubbed out cleanup script stages.

---
+++ tasks/db_cleanup.js
@@ -0,0 +1,11 @@
+/*
+  Task to clean up unused content from the database
+*/
+
+console.log('starting cleanup...');
+
+console.log('checking for expired temp boards...');
+
+console.log('checking for expired temp users...');
+
+console.log('cleanup finished.')
