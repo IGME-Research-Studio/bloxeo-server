e21511417e6e140440fc2987ad524e78761bac48
Eric Kipnis <esk8107@rit.edu>
2015-12-02 13:58:58 -0500

    Add Unit tests for timer and state service

---
+++ test/unit/services/StateService.test.js
@@ -0,0 +1,39 @@
+import Monky from 'monky';
+import chai from 'chai';
+import database from '../../../api/services/database';
+import StateService from '../../../api/services/StateService';
+
+const expect = chai.expect;
+const mongoose = database();
+const monky = new Monky(mongoose);
+
+mongoose.model('Board', require('../../../api/models/Board.js').schema);
+monky.factory('Board', {boardId: '1'});
+
+describe('StateService', function() {
+
+  before((done) => {
+    database(done);
+  });
+
+  describe('#setState(boardId, state)', () => {
+    it('Should set the state of the board in Redis', (done) => {
+      StateService.setState('1', StateService.StateEnum.createIdeasAndIdeaCollections)
+      .then((result) => {
+        expect(result).to.be.true;
+        done();
+      });
+    });
+  });
+
+  describe('#getState(boardId)', () => {
+    it('Should get the state of the board from Redis', (done) => {
+      StateService.getState('1')
+      .then((result) => {
+        expect(result).to.be.an('object');
+        console.log(result);
+        done();
+      });
+    });
+  });
+});
