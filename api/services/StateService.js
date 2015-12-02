2f43e2d924e586e293b8d9d55a01d04fe2face38
Eric Kipnis <esk8107@rit.edu>
2015-12-02 13:58:58 -0500

    Add Unit tests for timer and state service

--- api/services/StateService.js
+++ api/services/StateService.js
@@ -4,6 +4,8 @@
   @file Contains logic for controlling the state of a board
 */
 const RedisService = require('./RedisService');
+// const EXT_EVENTS = require('../constants/EXT_EVENT_API');
+// const stream = require('../event-stream');
 const stateService = {};
 
 stateService.StateEnum = {
@@ -29,11 +31,20 @@ stateService.StateEnum = {
 
 /**
 * Set the current state of the board on Redis.
+* Returns a promise containing the boolean showing the success of setting the state
 * @param {string} boardId: The string id generated for the board (not the mongo id)
 * @param {StateEnum} state: The state object to be set on Redis
 */
 stateService.setState = function(boardId, state) {
-  RedisService.set(boardId, JSON.stringify(state));
+  return RedisService.set(boardId + '-state', JSON.stringify(state))
+  .then(function(result) {
+    if (result.toLowerCase() === 'ok') {
+      return true;
+    }
+    else {
+      return false;
+    }
+  });
 };
 
 /**
@@ -41,9 +52,9 @@ stateService.setState = function(boardId, state) {
 * @param {string} boardId: The string id generated for the board (not the mongo id)
 */
 stateService.getState = function(boardId) {
-  return RedisService.get(boardId).then(function(result) {
+  return RedisService.get(boardId + '-state').then(function(result) {
     if (result !== undefined) {
-      return result;
+      return JSON.parse(result);
     }
     else {
       this.setState(boardId, this.StateEnum.createIdeasAndIdeaCollections);
