5b8605d42b6a5ee4f756db4a5bf1ba8fe8ccf45b
Braxton <baf9201@rit.edu>
2015-12-03 15:43:21 -0500

    Add redisService and update VotingService.
    Includes updates to setUserReady, isRoomReady, isUserReady, getResults,
    and getvotesleft.

---
+++ api/services/RedisService.js
@@ -0,0 +1,10 @@
+/**
+  Redis Service
+  @file Creates a singleton for a Redis connection
+*/
+const Redis = require('ioredis');
+const config = require('../../config');
+const redisURL = config.redisURL;
+const redis = new Redis(redisURL);
+
+module.exports = redis;
