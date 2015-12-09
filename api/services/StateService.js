c4033e0caac73887020a12a40284a118db951e35
Eric Kipnis <esk8107@rit.edu>
2015-12-09 18:29:47 -0500

    Finish State and Timer services.

--- api/services/StateService.js
+++ api/services/StateService.js
@@ -4,6 +4,7 @@
   @file Contains logic for controlling the state of a board
 */
 const RedisService = require('./RedisService');
+const Promise = require('bluebird');
 // const EXT_EVENTS = require('../constants/EXT_EVENT_API');
 // const stream = require('../event-stream');
 const stateService = {};
@@ -29,21 +30,41 @@ stateService.StateEnum = {
   },
 };
 
+function checkRequiresAdmin(requiresAdmin, boardId, userToken) {
+  return new Promise((resolve) => {
+    if (requiresAdmin) {
+      isAdmin(boardId, userToken)
+      .then((result) => {
+        resolve(result);
+      });
+    }
+    else {
+      resolve(false);
+    }
+  });
+}
+
 /**
 * Set the current state of the board on Redis.
 * Returns a promise containing the boolean showing the success of setting the state
 * @param {string} boardId: The string id generated for the board (not the mongo id)
 * @param {StateEnum} state: The state object to be set on Redis
 */
-stateService.setState = function(boardId, state) {
-  return RedisService.set(boardId + '-state', JSON.stringify(state))
-  .then(function(result) {
-    if (result.toLowerCase() === 'ok') {
-      return true;
-    }
-    else {
-      return false;
-    }
+stateService.setState = function(boardId, state, requiresAdmin, userToken) {
+  checkRequiresAdmin(requiresAdmin, userToken)
+  .then(() => {
+    return RedisService.set(boardId + '-state', JSON.stringify(state))
+    .then((result) => {
+      if (result.toLowerCase() === 'ok') {
+        return true;
+      }
+      else {
+        return false;
+      }
+    });
+  })
+  .catch((err) => {
+    throw err;
   });
 };
 
@@ -61,6 +82,18 @@ stateService.getState = function(boardId) {
       return this.StateEnum.createIdeasAndIdeaCollections;
     }
   });
+};
+
+stateService.createIdeasAndIdeaCollections = function(boardId, requiresAdmin, userToken) {
+  this.setState(boardId, this.StateEnum.createIdeasAndIdeaCollections, requiresAdmin, userToken);
+};
+
+stateService.createIdeaCollections = function(boardId, requiresAdmin, userToken) {
+  this.setState(boardId, this.StateEnum.createIdeaCollections, requiresAdmin, userToken);
+};
+
+stateService.voteOnIdeaCollections = function(boardId, requiresAdmin, userToken) {
+  this.setState(boardId, this.StateEnum.voteOnIdeaCollections, requiresAdmin, userToken);
 };
 
 module.exports = stateService;
--- api/services/TimerService.js
+++ api/services/TimerService.js
@@ -13,6 +13,16 @@ const EXT_EVENTS = require('../constants/EXT_EVENT_API');
 const stream = require('../event-stream').default;
 const timerService = {};
 
+dt.on('event', function(eventData) {
+  stream.ok(EXT_EVENTS.TIMER_EXPIRED, eventData, boardId);
+});
+
+dt.join(function(err) {
+  if (err) {
+    throw new Error(err);
+  }
+});
+
 /**
 * Returns a promise containing a boolean if the timer started correctly
 * @param {string} boardId: The string id generated for the board (not the mongo id)
@@ -21,28 +31,16 @@ const timerService = {};
 */
 
 timerService.startTimer = function(boardId, timerLengthInMilliseconds) {
-
-  dt.on('event', function(eventData) {
-    console.log('Event completed: ' + eventData.key);
-  });
-
-  dt.join(function(err) {
-    if (err) {
-      reject(new Error(err));
-    }
-  });
-
   return new Promise(function(resolve, reject) {
     try {
-      dt.post({key: boardId}, timerLengthInMilliseconds, function(err, eventId) {
+      dt.post({boardId: boardId}, timerLengthInMilliseconds, function(err, eventId) {
         if (err) {
           reject(new Error(err));
         }
-        stream.emit(EXT_EVENTS.START_TIMER, {boardId});
         resolve(eventId);
       });
     }
-    catch(e) {
+    catch (e) {
       reject(e);
     }
   });
@@ -53,38 +51,19 @@ timerService.startTimer = function(boardId, timerLengthInMilliseconds) {
 * @param {string} boardId: The string id generated for the board (not the mongo id)
 */
 timerService.stopTimer = function(boardId, eventId) {
-
   return new Promise(function(resolve, reject) {
     try {
       dt.cancel(eventId, function(err) {
         if (err) {
           reject(err);
         }
-        console.log('canceled the timer for event id: ' + eventId);
         resolve(true);
       });
     }
-    catch(e) {
+    catch (e) {
       reject(e);
     }
-  })
+  });
 };
-
-/**
-* Returns a promise containing the time to live (ttl) of the key in seconds
-* @param {string} boardId: The string id generated for the board (not the mongo id)
-* @return: rturns 0 if the key doesn't exist or expired
-*/
-// timerService.getTimeLeft = function(boardId) {
-//   const timerId = boardId + iDExtenstion;
-//   return RedisService.ttl(timerId).then(function(result) {
-//     if (result <= 0) {
-//       return 0;
-//     }
-//     else {
-//       return result;
-//     }
-//   });
-// };
 
 module.exports = timerService;
