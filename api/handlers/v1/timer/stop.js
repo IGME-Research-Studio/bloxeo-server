b8fc7651718a4ff465e78bfc2af085245432f447
Eric Kipnis <esk8107@rit.edu>
2015-12-09 18:29:47 -0500

    Finish State and Timer services.

--- api/handlers/v1/timer/stop.js
+++ api/handlers/v1/timer/stop.js
@@ -8,27 +8,27 @@
 */
 
 import { isNull } from '../../../services/ValidatorService';
-import { startTimer } from '../../../services/TimerService';
+import { stopTimer } from '../../../services/TimerService';
 import EXT_EVENTS from '../../../constants/EXT_EVENT_API';
 import stream from '../../../event-stream';
 
-export default function startTimerCountdown(req) {
+export default function disableTimer(req) {
   const socket = req.socket;
   const boardId = req.boardId;
-  const timerLengthInMilliseconds = req.timerLengthInMilliseconds;
+  const eventId = req.eventId;
 
   if (isNull(socket)) {
     throw new Error('Undefined request socket in handler');
   }
-  else if (isNull(boardId) || isNull(timerLengthInMilliseconds)) {
-    stream.badRequest(EXT_EVENTS.STARTED_TIMER, {}, socket,
+  else if (isNull(boardId) || isNull(eventId)) {
+    stream.badRequest(EXT_EVENTS.DISABLED_TIMER, {}, socket,
       'Not all required parameters were supplied');
   }
   else {
     // @todo pass user along
-    startTimer(boardId, timerLengthInMilliseconds)
-      .then((eventId) => stream.OK(EXT_EVENTS.START_TIMER, {eventId}, boardId))
-      .catch((err) => stream.serverError(EXT_EVENTS.START_TIMER,
+    stopTimer(boardId, eventId)
+      .then((success) => stream.ok(EXT_EVENTS.DISABLED_TIMER, {boardId: boardId, disabled: success}, boardId))
+      .catch((err) => stream.serverError(EXT_EVENTS.DISABLED_TIMER,
                                          err.message, socket));
   }
 }
