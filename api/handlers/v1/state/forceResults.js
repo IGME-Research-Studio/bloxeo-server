cc12d7caf784414d8912ac281c071001a5a6f0d3
Will <whp3652@rit.edu>
2015-12-16 13:55:41 -0500

    Refactor the new handlers and fix some typos

--- api/handlers/v1/state/forceResults.js
+++ api/handlers/v1/state/forceResults.js
@@ -9,25 +9,26 @@
 
 import { isNull } from '../../../services/ValidatorService';
 import { createIdeaCollections } from '../../../services/StateService';
-import EXT_EVENTS from '../../../constants/EXT_EVENT_API';
+import { FORCED_RESULTS } from '../../../constants/EXT_EVENT_API';
 import stream from '../../../event-stream';
 
 export default function forceResults(req) {
-  const socket = req.socket;
-  const boardId = req.boardId;
-  const token = req.userToken;
+  const { socket, boardId, userToken } = req;
 
   if (isNull(socket)) {
     throw new Error('Undefined request socket in handler');
   }
-  else if (isNull(boardId) || isNull(token)) {
-    stream.badRequest(EXT_EVENTS.FORCED_RESULTS, {}, socket,
-      'Not all required parameters were supplied');
+
+  if (isNull(boardId) || isNull(userToken)) {
+    return stream.badRequest(FORCED_RESULTS, {}, socket);
   }
-  else {
-    createIdeaCollections(boardId, true, token)
-      .then((state) => stream.ok(EXT_EVENTS.FORCED_RESULTS, {boardId: boardId, state: state}, boardId))
-      .catch((err) => stream.serverError(EXT_EVENTS.FORCED_RESULTS,
-                                         err.message, socket));
-  }
+
+  return createIdeaCollections(boardId, true, userToken)
+    .then((state) => {
+      return stream.ok(FORCED_RESULTS, {boardId: boardId, state: state},
+                       boardId);
+    })
+    .catch((err) => {
+      return stream.serverError(FORCED_RESULTS, err.message, socket);
+    });
 }
