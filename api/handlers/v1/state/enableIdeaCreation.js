87b9277b5db79da6efc2da9fba7314c4bf0d31f1
Will <whp3652@rit.edu>
2015-12-16 15:06:04 -0500

    Implement userToken parsing for all new handlers

--- api/handlers/v1/state/enableIdeaCreation.js
+++ api/handlers/v1/state/enableIdeaCreation.js
@@ -7,13 +7,17 @@
 * @param {string} req.userToken
 */
 
+import R from 'ramda';
+import { JsonWebTokenError } from 'jsonwebtoken';
 import { isNull } from '../../../services/ValidatorService';
+import { verifyAndGetId } from '../../../services/TokenService';
 import { createIdeasAndIdeaCollections } from '../../../services/StateService';
 import { ENABLED_IDEAS } from '../../../constants/EXT_EVENT_API';
 import stream from '../../../event-stream';
 
 export default function enableIdeaCreation(req) {
   const { socket, boardId, userToken } = req;
+  const setState = R.partial(createIdeasAndIdeaCollections, [boardId, true]);
 
   if (isNull(socket)) {
     return new Error('Undefined request socket in handler');
@@ -22,10 +26,14 @@ export default function enableIdeaCreation(req) {
     return stream.badRequest(ENABLED_IDEAS, {}, socket);
   }
 
-  return createIdeasAndIdeaCollections(boardId, true, userToken)
+  return verifyAndGetId(userToken)
+    .then(setState)
     .then((state) => {
       return stream.ok(ENABLED_IDEAS, {boardId: boardId, state: state},
                        boardId);
+    })
+    .catch(JsonWebTokenError, (err) => {
+      return stream.unauthorized(ENABLED_IDEAS, err.message, socket);
     })
     .catch((err) => {
       return stream.serverError(ENABLED_IDEAS, err.message, socket);
--- api/handlers/v1/state/forceResults.js
+++ api/handlers/v1/state/forceResults.js
@@ -7,26 +7,33 @@
 * @param {string} req.userToken
 */
 
+import R from 'ramda';
+import { JsonWebTokenError } from 'jsonwebtoken';
 import { isNull } from '../../../services/ValidatorService';
+import { verifyAndGetId } from '../../../services/TokenService';
 import { createIdeaCollections } from '../../../services/StateService';
 import { FORCED_RESULTS } from '../../../constants/EXT_EVENT_API';
 import stream from '../../../event-stream';
 
 export default function forceResults(req) {
   const { socket, boardId, userToken } = req;
+  const setState = R.partial(createIdeaCollections, [boardId, true]);
 
   if (isNull(socket)) {
     return new Error('Undefined request socket in handler');
   }
-
   if (isNull(boardId) || isNull(userToken)) {
     return stream.badRequest(FORCED_RESULTS, {}, socket);
   }
 
-  return createIdeaCollections(boardId, true, userToken)
+  return verifyAndGetId(userToken)
+    .then(setState)
     .then((state) => {
       return stream.ok(FORCED_RESULTS, {boardId: boardId, state: state},
                        boardId);
+    })
+    .catch(JsonWebTokenError, (err) => {
+      return stream.unauthorized(FORCED_RESULTS, err.message, socket);
     })
     .catch((err) => {
       return stream.serverError(FORCED_RESULTS, err.message, socket);
