5420f61d53ef5c982cdc2a64b1c64cf077290a57
Will <whp3652@rit.edu>
2015-12-24 21:56:59 -0500

    Fix test error on IdeaCollectionService#removeDups
    Before each wasn't waiting for monky to finish

--- test/constants.js
+++ test/constants.js
@@ -3,13 +3,14 @@
  *
  * Import only the ones you need
  * @example
- * import {BOARID, RESULT_KEY} from './constants';
+ * import {BOARDID, RESULT_KEY} from './constants';
  */
 
 export const BOARDID = '1';
 export const BOARDID_2 = '2';
 export const USERNAME = 'brapnis#n';
 export const RESULT_KEY = 'result1';
-export const COLLECTION_KEY = 'collection1';
+export const COLLECTION_KEY = 'collection';
+export const COLLECTION_KEY_2 = 'collection2';
 export const IDEA_CONTENT = 'idea';
 export const IDEA_CONTENT_2 = 'idea2';
