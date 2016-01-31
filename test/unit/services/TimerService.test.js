a4ab3194cefaa9f96e1c825073c8c1a4dbacef2e
Eric Kipnis <esk8107@rit.edu>
2016-01-31 17:52:13 -0500

    Rework Timer Service to use Radicchio Library.
    Rework handlers and unit tests to use Radicchio library

--- test/unit/services/TimerService.test.js
+++ test/unit/services/TimerService.test.js
@@ -3,31 +3,51 @@ import TimerService from '../../../api/services/TimerService';
 
 describe('TimerService', function() {
 
-  describe('#startTimer(boardId, timerLengthInSeconds, (optional) value)', () => {
-    xit('Should start the server timer on Redis', (done) => {
-      TimerService.startTimer('1', 10, undefined)
-      .then((result) => {
-        expect(result).to.be.true;
+  describe('#startTimer(boardId, timerLengthInSeconds)', () => {
+    it('Should start the server timer', (done) => {
+      TimerService.startTimer('abc123', '10000')
+      .then((timerId) => {
+        expect(timerId).to.be.a('string');
         done();
       });
     });
   });
 
   describe('#stopTimer(boardId)', () => {
+    const timerObj = {};
+
+    beforeEach(function(done) {
+      TimerService.startTimer('abc123', '10000')
+      .then((timerId) => {
+        timerObj.timerId = timerId;
+        done();
+      });
+    });
+
     it('Should stop the server timer on Redis', (done) => {
-      TimerService.stopTimer('1')
-      .then((result) => {
-        expect(result).to.be.true;
+      TimerService.stopTimer(timerObj.timerId)
+      .then((timerDataObj) => {
+        expect(timerDataObj).to.be.an('object');
         done();
       });
     });
   });
 
   describe('#getTimeLeft(boardId)', () => {
-    xit('Should get the time left on the sever timer from Redis', (done) => {
-      TimerService.getTimeLeft('1')
-      .then((result) => {
-        expect(result).to.be.a('number');
+    const timerObj = {};
+
+    beforeEach(function(done) {
+      TimerService.startTimer('abc123', '10000')
+      .then((timerId) => {
+        timerObj.timerId = timerId;
+        done();
+      });
+    });
+
+    it('Should get the time left on the sever timer from Redis', (done) => {
+      TimerService.getTimeLeft(timerObj.timerId)
+      .then((timeLeft) => {
+        expect(timeLeft).to.be.a('number');
         done();
       });
     });
