15311c664fb05da98462afdf72ba96f0f1e5a519
Will <whp3652@rit.edu>
2016-02-27 18:44:31 -0500

    Change what data is encoded in a userToken
    Add some extra data checks to a couple handlers

--- api/handlers/v1/constants/index.js
+++ api/handlers/v1/constants/index.js
@@ -5,12 +5,17 @@
 * @param {Object} req.socket the connecting socket object
 */
 
+import { isNil } from 'ramda';
 import constantsService from '../../../services/ConstantsService';
 import { RECEIVED_CONSTANTS } from '../../../constants/EXT_EVENT_API';
 import stream from '../../../event-stream';
 
 export default function index(req) {
   const { socket } = req;
+
+  if (isNil(socket)) {
+    return new Error('Undefined request socket in handler');
+  }
 
   return stream.emitTo({event: RECEIVED_CONSTANTS,
                  code: 200,
