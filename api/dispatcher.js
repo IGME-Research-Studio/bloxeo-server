4b427b5cafb1ecc131ac0705f982163a022d63b1
Will <whp3652@rit.edu>
2016-02-29 16:36:43 -0500

    User join/leave now waits for socket.io to finish

--- api/dispatcher.js
+++ api/dispatcher.js
@@ -51,29 +51,24 @@ const dispatcher = function(server) {
    * @param {Object} req should be of the form:
    * {boardId, userId, socket, res,
    *   cbRes: {event, res}}
-   *
-   * @XXX the broadcast that the user joined should wait for the success
-   * callback of the join operation.
    */
   stream.on(JOIN, (req) => {
     log.info(JOIN, req.boardId, req.userId);
-    req.socket.join(req.boardId);
-    io.in(req.boardId).emit(req.cbRes.event, req.cbRes.res);
+    req.socket.join(req.boardId, function() {
+      io.in(req.boardId).emit(req.cbRes.event, req.cbRes.res);
+    });
   });
 
   /**
    * @param {Object} req should be of the form:
    * {boardId, userId, socket, res,
    *   cbRes: {event, res}}
-   *
-   * @XXX the broadcast that the user left should wait for the success
-   * callback of the leave operation. This would handle disconnections
-   * as well.
    */
   stream.on(LEAVE, (req) => {
     log.info(LEAVE, req.boardId, req.userId);
-    req.socket.leave(req.boardId);
-    io.in(req.boardId).emit(req.cbRes.event, req.cbRes.res);
+    req.socket.leave(req.boardId, function() {
+      io.in(req.boardId).emit(req.cbRes.event, req.cbRes.res);
+    });
   });
 };
 
