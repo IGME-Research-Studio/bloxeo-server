efb03df4ec5fe21c3b145404195d5e4c871b6dea
Will <whp3652@rit.edu>
2016-03-03 14:50:45 -0500

    Don't require name and description to create board

--- api/controllers/v1/boards/create.js
+++ api/controllers/v1/boards/create.js
@@ -4,20 +4,20 @@
  */
 
 import { isNil } from 'ramda';
-import BoardService from '../../../services/BoardService';
 import { verifyAndGetId } from '../../../services/TokenService';
+import { create as createBoard } from '../../../services/BoardService';
 
 export default function create(req, res) {
-  const { userToken, name, description } = req;
+  const { userToken, name, description } = req.body;
 
-  if (isNil(userToken) || isNil(name) || isNil(description)) {
+  if (isNil(userToken)) {
     return res.badRequest(
       {message: 'Not all required parameters were supplied'});
   }
 
   return verifyAndGetId(userToken)
   .then((userId) => {
-    BoardService.create(userId, name, description)
+    createBoard(userId, name, description)
       .then((boardId) => res.created({boardId: boardId}))
       .catch((err) => res.serverError(err));
   });
