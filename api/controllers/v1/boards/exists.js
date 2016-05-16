33774deba72eb2e2b7b891e51260734fbf256f6b
Eric Kipnis <esk8107@rit.edu>
2016-05-16 13:42:22 -0400

    Create exists handler for board to check for 404

--- api/controllers/v1/boards/exists.js
+++ api/controllers/v1/boards/exists.js
@@ -1,1 +1,23 @@
-// board id, return true/false on successful call
+/**
+ * BoardController#exists
+ *
+ * Checks if a board exists through an HTTP request.
+ */
+
+import { values } from 'ramda';
+import { exists as doesBoardExist } from '../../../services/BoardService';
+import { anyAreNil } from '../../../helpers/utils';
+
+export default function create(req, res) {
+  const { boardId } = req.body;
+  const required = { boardId };
+
+  if (anyAreNil(values(required))) {
+    return res.badRequest({ ...required,
+      message: 'Not all required parameters were supplied'});
+  }
+
+  return doesBoardExist(boardId)
+    .then((boardExists) => res.ok({exists: boardExists}))
+    .catch((err) => res.serverError(err));
+}
